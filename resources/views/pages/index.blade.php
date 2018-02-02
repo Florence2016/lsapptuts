5c8903dbdfb2c9cc52b3235ce4e16d2d3691d9e2
Florence Suller <FlorenceSuller@gmail.com>
2018-02-02 09:17:29 +0800

    larafellow added

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="jumbotron text-center">
        <h1>Welcome to Laravel</h1>
-       <p>Learn laravel in basic to advanced.This is your journey begins You can do it</p>
+       <p>Learn laravel in basic to advanced.This is your journey begins. Hey Larafellow You can do it</p>
        <p><a class="btn btn-primary btn-lg" href="/login" role ="button">Login</a> <a class = "btn btn-success btn-lg" href="/register" role="button">Register</a></p>
 </div>
 @endsection
